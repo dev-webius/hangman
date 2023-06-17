2b1b9a0c2ce7bd87abadca5e696feb5154d55f99
KimSangBum101 <rlatkdqja101@naver.com>
2023-06-17 14:20:55 +0900

    Feat: Game Status 상수 정의

--- src/slices/gameSlice.js
+++ src/slices/gameSlice.js
@@ -1,8 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+export const GAME_STATUS = {
+  READY: 0,
+  PLAYING: 1,
+  VICTORY: 2,
+  DEFEAT: 3,
+};
+
 const name = 'game';
 const initialState = {
-  status: '',
+  status: GAME_STATUS.READY,
   failCount: 0,
   systemWord: [], // 랜덤 추첨된 정답 글자 배열
   answerWord: [], // 현재 맞춘 글자 배열
