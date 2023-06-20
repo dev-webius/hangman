ae48adeb7cb782eff73392a3b8c182146f03617b
juheexuiux <juheexuiux@gmail.com>
2023-06-20 16:34:19 +0900

    Chore: Hangman images upload

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,10 +1,23 @@
-// import image01 from './../../images/hanmgman01.png';
+import image01 from './images/hangman01.png';
+import image02 from './images/hangman02.png';
+import image03 from './images/hangman03.png';
+import image04 from './images/hangman04.png';
+import image05 from './images/hangman05.png';
+import image06 from './images/hangman06.png';
+import image07 from './images/hangman07.png';
+import image08 from './images/hangman08.png';
+import image09 from './images/hangman09.png';
+import image10 from './images/hangman10.png';
+import image11 from './images/hangman11.png'
 
 function Hangman() {
-  return
-  <>
-    {/* <img src={image01}> */}
-  </>;
+  return (
+    <>
+      <div className='hangman'>
+        <img src={image11} alt="" />
+      </div>
+    </>
+  );
 }
 
 export default Hangman;
